926c6ab172a79610867fbb7310fd1d6c93b16bc5
Balram Pandey <balrampandey@Balrams-MacBook-Air.local>
2022-04-29 18:46:37 +0530

    fix

--- app/src/main/java/com/example/redcptsdksample/LoginActivity.kt
+++ app/src/main/java/com/example/redcptsdksample/LoginActivity.kt
@@ -2,80 +2,30 @@ package com.example.redcptsdksample
 
 import android.os.Bundle
 import android.transition.TransitionManager
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.postDelayed
-import com.redcpt.sdk.RedCpt
-import com.redcpt.sdk.otp.Otp
-import com.redcpt.sdk.otp.models.SignUpResponse
-import com.redcpt.sdk.otp.models.VerifyResponse
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var sdk: RedCpt
-    private lateinit var otp: Otp
+
     private var signupRetryCount: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-        sdk = (application as SampleApplication).sdk
-        otp = sdk.getOtpInstance()
-
         requestPhoneNumberState()
 
-        btSignup.setOnClickListener {
-            val phoneNumber = etPhone.text.toString()
-            waitingState()
-            isOtpVerified()
-            otp.signup(phoneNumber, ++signupRetryCount, object : Otp.SignupCallback {
 
-                override fun onError(response: SignUpResponse?, error: Throwable?) {
-                    when {
-                        error != null -> phoneNumberErrorState(error.message)
-                        response != null -> phoneNumberErrorState(response.message)
-                    }
-                }
 
-                override fun onSuccess(response: SignUpResponse) {
-                    requestOtpState()
-                }
 
-            })
-        }
-
-        btVerify.setOnClickListener {
-            val phoneNumber = etPhone.text.toString()
-            val password = etOtp.text.toString()
-            waitingState()
-
-            otp.verify(phoneNumber, password, object : Otp.VerifyCallback {
-                override fun onError(response: VerifyResponse?, error: Throwable?) {
-                    when {
-                        error != null -> otpErrorState(error.message)
-                        response != null -> otpErrorState(response.message)
-                    }
-                }
-
-                override fun onSuccess(verifyResponse: VerifyResponse) {
-                    loginSuccessfulState()
-                    loginRoot.postDelayed(1000) {
-                        finish()
-                    }
-                }
-            })
-        }
 
     }
 
 
     private fun isOtpVerified(): Boolean {
-        Log.e("isOtpVerified", sdk.isOtpVerified().toString())
-        return sdk.isOtpVerified()
+        return true
     }
 
     private fun waitingState() {
--- app/src/main/java/com/example/redcptsdksample/SampleApplication.kt
+++ app/src/main/java/com/example/redcptsdksample/SampleApplication.kt
@@ -1,16 +1,15 @@
 package com.example.redcptsdksample
 
 import android.app.Application
-import com.redcpt.sdk.RedCpt
 
 class SampleApplication : Application() {
 
-    lateinit var sdk: RedCpt
-        private set
+//    lateinit var sdk: RedCpt
+//        private set
 
     override fun onCreate() {
         super.onCreate()
-        sdk = RedCpt.init(this, "4d0de8ca9539459fb3a3b1d56b1f6f19")
+//        sdk = RedCpt.init(this, "4d0de8ca9539459fb3a3b1d56b1f6f19")
     }
 
 }
